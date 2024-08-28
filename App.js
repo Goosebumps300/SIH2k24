340af31d0c8cfb054821776fa2d084eae15f223c
Goosebumps300 <sandipandebnath2002@gmail.com>
2024-08-28 21:07:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) =>{
-    res.send("hello world");
+    res.send("Hello");
 })
 
 app.listen(3000);
